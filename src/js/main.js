08869e32b8e198c4adf23efba6d714cb559f6105
Shroomy Shroom <sporeventexplosion@gmail.com>
2015-06-27 18:36:48 +0300

    Add code for app opening

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,7 @@
   // App class
 
   var App = function (id, path, options) {
-    this.id = name;
+    this.id = id;
     this.path = path;
     this.options = options || {};
     // Default app name
@@ -24,23 +24,40 @@
     var appFrameWindow = appFrame.contentWindow;
     PC.appFrameWindow = appFrameWindow, PC.appFrame = appFrame;
     PC.currentAppName = document.querySelector(".app-name");
-    // Open the app store by default
-    PC.apps.open(PC.apps.appList[0]);
+    var appCenter = PC.apps.getById("pegasus-app-center");
+    PC.apps.open(appCenter);
   };
 
   PC.apps = {};
 
-  // Temporary list of apps. Will be removed in the future.
-
   PC.apps.appList = [];
   // Temporary app store app
-  PC.apps.appList.push(new PC.App("pegasus-app-center", "/apps/appcenter/index.html", {"name": "Pegasus App Center"}));
+
 
   PC.apps.open = function (app) {
     PC.appFrame.src = app.path;
     // Set app name
     PC.currentAppName.textContent = app.options.name;
   };
+
+  PC.apps.add = function (app) {
+    PC.apps.appList.push(app);
+  };
+
+  PC.apps.getById = function (id) {
+    for (var i = 0; i < PC.apps.appList.length; i++) {
+      if (PC.apps.appList[i].id === id) {
+        return PC.apps.appList[i];
+      }
+    }
+  };
+
+  // Temporary code
+
+  PC.apps.add(new PC.App("pegasus-app-center", "/apps/appcenter/index.html", {"name": "Pegasus App Center"}));
+
+  // App store app. Will later be stored in a database
+
 
   addEventListener("DOMContentLoaded", PC.main, false);
 
