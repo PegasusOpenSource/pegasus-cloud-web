0be740f1f686650d308ae198416fce70b9948fb8
Shroomy Shroom <sporeventexplosion@gmail.com>
2015-07-04 20:05:18 +0300

    add app object

---
+++ src/js/components/app.js
@@ -0,0 +1,23 @@
+define([], function () {
+  var App = function (appID, settings) {
+    // Use the settings.id as appID if only a settings object is passed
+    typeof appID === "object" && (settings = appID, appID = settings.ID);
+
+    // Throw error if appID is not a string
+
+    if (typeof appID !== "string") {
+      throw new TypeError("appID must be a string");
+    }
+
+    this.appID = appID;
+    this.settings = settings;
+
+    // Use default name if app doesn't have a name or name is falsey
+    this.name = settings.name ? settings.name : "Untitled Cloud App";
+    // App directory
+    this.dir = settings.appDir;
+  };
+
+  // Set app to global
+  window.App = App;
+});
--- src/js/core.js
+++ src/js/core.js
@@ -1,5 +1,6 @@
 define([
-  "pc"
+  "pc",
+  "components/app"
   ], function(pc){
     return pc;
 });
